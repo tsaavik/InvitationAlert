8f7e622a4096ab94d488deecfbe187a6e1b5a80d
David Mcanulty <github@hellspark.com>
2022-10-28 19:51:26 +0000

    Update Simplified code and fixed lint warnings

--- InvitationAlert.lua
+++ InvitationAlert.lua
@@ -1,6 +1,11 @@
-local lastStatus = false
+-- aia = Audio Invitation Alert?
+local aiaDebug = false
 
-local function BGEvetHandle()
+local lastStatus = false
+local function BGEventHandle()
+	-- This handles PVP notifications. Still figuring it out
+	-- GetBattlefieldPortExpiration - seconds before battlefield port expires
+	-- GetMaxBattlefieldID() - Returns max numer of battlefields you can queue
 	if PVPReadyDialog:IsShown() then	
 		local qStatus = false 
 		for i=1, GetMaxBattlefieldID() do
@@ -11,80 +16,102 @@ local function BGEvetHandle()
 		end
 		
 		if qStatus == true and lastStatus == false then 
-			aiaEvent()
+			AiaEvent()
 		end
 		
 		lastStatus = qStatus
 	end	
 end
 
-
-function aiaInit()
+function AiaInit()
+	-- This hooks us into the LFG and PVP ready events
 	frmInvitationAlert:RegisterEvent("LFG_PROPOSAL_SHOW");
-	PVPReadyDialog:HookScript("OnUpdate", BGEvetHandle); 
+	PVPReadyDialog:HookScript("OnUpdate", BGEventHandle); 
 end
 
 local aiatiks= 0;
 local aiatimer = nil
 local aiastatus = false;
-aiaSEnableSoundWhenGameIsInBG = 1;
-aiaSEnableAllSound = 1;
-aiaSEnableSFX = 1;
-aiaSMasterVolume = 0.3;
-aiaSSFXVolume = 0.3;
-
-function aiaUpdate()
+local aiaSEnableSoundWhenGameIsInBG = true;
+local aiaSEnableAllSound = true;
+local aiaSEnableSFX = true;
+local aiaSMasterVolume = 0.3;
+local aiaSSFXVolume = 0.3;
+function AiaUpdate()
+	-- Entire function only runs when aiastatus has been set true by AiaEvent
+    -- Restores enviroment after sound has played via AiaEvent
 	if (aiastatus == true) then
 		if not aiatimer then
 			aiatimer = GetTime() + 2 
 		end
-		if (GetTime()> aiatimer) then	--180	
-			--print ("back settings");
-			--print (aiaSEnableAllSound);
-			--print (aiaSEnableSFX);
-			--print (aiaSMasterVolume);
-			--print (aiaSSFXVolume);
+		if (GetTime()> aiatimer) then
+			if aiaDebug	then
+				print ("Restoring user sound settings");
+				print ("EnableAllSound: ", aiaSEnableAllSound);
+				print ("EnableSFX: ", aiaSEnableSFX);
+				print ("MasterVolume: ", aiaSMasterVolume);
+				print ("SFXVolume: ", aiaSSFXVolume);
+				print ("Sound_EnableSoundWhenGameIsInBG: ", aiaSEnableSoundWhenGameIsInBG)
+			end
 			SetCVar("Sound_EnableAllSound", aiaSEnableAllSound);
 			SetCVar("Sound_EnableSFX", aiaSEnableSFX);
-			SetCVar("Sound_MasterVolume", aiaSMasterVolume);
-			SetCVar("Sound_SFXVolume", aiaSSFXVolume);
-			
+			SetCVar("Sound_MasterVolume", tostring(aiaSMasterVolume));
+			SetCVar("Sound_SFXVolume", tostring(aiaSSFXVolume));
+			SetCVar("Sound_EnableSoundWhenGameIsInBG", aiaSEnableSoundWhenGameIsInBG);
 			aiastatus = false;
 			aiatimer = nil
 		end
 	end
 end
 
-function aiaEvent()
-aiastatus = true; 
-aiatiks = 0; 
+function AiaEvent()
+   -- We've gotten the signal, save the users current sound settings and fire alert at high volume
+   aiastatus = true;  -- so we can trigger AiaUpdate to restore the settings when we are done
+   aiatiks = 0; 
 
---aiaSEnableSoundWhenGameIsInBG = GetCVar("Sound_EnableSoundWhenGameIsInBG");
-aiaSEnableAllSound = GetCVar("Sound_EnableAllSound");
-aiaSEnableSFX = GetCVar("Sound_EnableSFX");
-aiaSMasterVolume = GetCVar("Sound_MasterVolume");
-aiaSSFXVolume = GetCVar("Sound_SFXVolume");
---print (aiaSEnableAllSound);
---print (aiaSEnableSFX);
---print (aiaSMasterVolume);
---print (aiaSSFXVolume);
-SetCVar("Sound_EnableSoundWhenGameIsInBG", 1);
-SetCVar("Sound_EnableAllSound", 1);
-SetCVar("Sound_EnableSFX", 1);
+   -- Capture user's sound settings for later restoriation
+   aiaSEnableSoundWhenGameIsInBG = GetCVar("Sound_EnableSoundWhenGameIsInBG");
+   aiaSEnableAllSound = GetCVar("Sound_EnableAllSound");
+   aiaSEnableSFX = GetCVar("Sound_EnableSFX");
+   aiaSMasterVolume = tonumber(GetCVar("Sound_MasterVolume"));
+   aiaSSFXVolume = tonumber(GetCVar("Sound_SFXVolume"));
+   if aiaDebug then
+   	  print("EnableAllSound: ", aiaSEnableAllSound);
+   	  print("EnableSFX: ", aiaSEnableSFX);
+   	  print("MasterVolume: ", aiaSMasterVolume);
+   	  print("SFXVolume: ", aiaSSFXVolume);
+	  print("aiaSEnableSoundWhenGameIsInBG: ", aiaSEnableSoundWhenGameIsInBG);
+   end
 
-local Mv = (0.1 +  aiaSMasterVolume) * 2 
-if Mv > 1 then
-Mv = 1;
-end
-local Sv = (0.1 + aiaSSFXVolume) * 2 
-if Sv > 1 then
-Sv = 1;
-end
-SetCVar("Sound_MasterVolume", Mv);
-SetCVar("Sound_SFXVolume", Sv);
+   if aiaSEnableSoundWhenGameIsInBG == false then
+	-- Currently broken, WoW seems to not change the variable when window is not focused :(
+      SetCVar("Sound_EnableSoundWhenGameIsInBG", true);
+	  if aiaDebug then
+      	print("Enabling background sound override");
+      	print("aiaSEnableSoundWhenGameIsInBG: (should be true now) ", aiaSEnableSoundWhenGameIsInBG);
+	  end
+   end
 
-print("|cff0000ff############################################|r");
-print("You have received an invitation through the LFG");
-print("|cff0000ff############################################|r");
-PlaySoundFile("Interface\\AddOns\\InvitationAlert\\mysound.ogg");
+   -- turn on sound and pump up the volume  
+   SetCVar("Sound_EnableAllSound", true);
+   SetCVar("Sound_EnableSFX", true);
+
+   --Compute Master Volume, not to exceed 1 (100%)
+   local Mv = (0.1 +  aiaSMasterVolume) * 2 
+   if Mv > 1 then
+      Mv = 1;
+   end
+
+   --Compute SFX Volume, not to exceed 1 (100%)
+   local Sv = (0.1 + aiaSSFXVolume) * 2 
+      if Sv > 1 then
+      Sv = 1;
+   end
+   SetCVar("Sound_MasterVolume", tostring(Mv));
+   SetCVar("Sound_SFXVolume", tostring(Sv));
+
+   print("|cff0000ff############################################|r");
+   print("You have received an invitation through the LFG");
+   print("|cff0000ff############################################|r");
+   PlaySoundFile("Interface\\AddOns\\InvitationAlert\\mysound.ogg");
 end
